3739b900ee072a677f93f2d55452953bda7b0f00
iamdavi <domato99.dom@gmail.com>
2023-02-17 22:15:50 +0100

    Rename files

--- parsers/2022/parser_provisional.php
+++ parsers/2022/parser_provisional.php
@@ -6,8 +6,8 @@ include_once(__DIR__ . '\.\classes\TestFile.php');
  * @link https://www.mjusticia.gob.es/es/Ciudadano/EmpleoPublico/Documents/PLANTILLA%20PROVISIONAL%202022.1%20-%20CASTELLANO.pdf
  * @param string Path of the content file
  */
-$file_name = __DIR__ . '\..\resources\2022\txt\text.txt';
-$output_file_name = __DIR__ . '\..\resources\2022\json\2022.json';
+$file_name = __DIR__ . '\..\resources\2022\txt\text_provisional.txt';
+$output_file_name = __DIR__ . '\..\resources\2022\json\2022_provisional.json';
 
 $correct_answers = [
 	'13110202210212210033102112001321020222031231302120232101',
