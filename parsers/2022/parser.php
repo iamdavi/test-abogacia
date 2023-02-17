be7328bb5d5278ff98a142baf5479836a196dd32
iamdavi <domato99.dom@gmail.com>
2023-02-17 21:39:26 +0100

    Add link to text

--- parsers/2022/parser.php
+++ parsers/2022/parser.php
@@ -2,8 +2,12 @@
 
 include_once(__DIR__ . '\.\classes\TestFile.php');
 
-$output_file_name = __DIR__ . '\..\resources\2022\json\2022.json';
+/**
+ * @link https://www.mjusticia.gob.es/es/Ciudadano/EmpleoPublico/Documents/PLANTILLA%20PROVISIONAL%202022.1%20-%20CASTELLANO.pdf
+ * @param string Path of the content file
+ */
 $file_name = __DIR__ . '\..\resources\2022\txt\text.txt';
+$output_file_name = __DIR__ . '\..\resources\2022\json\2022.json';
 
 $file_parser = new TestFile($file_name);
 $file_parser->parseFile();
