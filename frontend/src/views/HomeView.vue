a0dd9cf49e79495b8521ea0feaec0876876a4501
iamdavi <domato99.dom@gmail.com>
2023-02-21 17:00:21 +0100

    Commit inicial de la parte de frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+	<main>
+		<v-container>
+			<v-row>
+				<v-col>
+					<h1 id="website-title">
+						Test de acceso a la abogacía
+					</h1>
+				</v-col>
+				<v-divider vertical></v-divider>
+				<v-col>
+				</v-col>
+			</v-row>
+		</v-container>
+	</main>
+</template>
+
+<style>
+</style>
